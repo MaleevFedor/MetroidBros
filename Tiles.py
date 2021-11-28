4f03a3c6faa75ca403909fbf138403bcdb33f04f
MaleevFedor <teddmail17@gmail.com>
2021-11-28 21:23:43 +0300

    Player can't fall out

--- Tiles.py
+++ Tiles.py
@@ -2,8 +2,8 @@ import pygame
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, pos, size):
+    def __init__(self, pos, sizeX, sizeY):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((size, size))
+        self.image = pygame.Surface((sizeX, sizeY))
         self.image.fill((0, 0, 0))
         self.rect = self.image.get_rect(topleft=pos)
