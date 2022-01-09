6a6701d5dc98c2c2e2ab16a3032802937d11b8a2
Wooster70 <ivantavrovsky@gmail.com>
2022-01-09 15:42:00 +0400

    level selector2

--- main.py
+++ main.py
@@ -42,6 +42,18 @@ def load_level():
     elif choiced == 'Plain':
         return GameWindow(PlainLevel(screen))
 
+def set_level(level):
+    if level == 'Forest':
+        return ForestLevel(screen)
+    elif level == 'Tokyo':
+        return TokyoLevel(screen)
+    elif level == 'Industrial':
+        return IndustrialLevel(screen)
+    elif level == 'Apocalypsis':
+        return ApocalypsisLevel(screen)
+    elif level == 'Plain':
+        return PlainLevel(screen)
+
 
 if __name__ == '__main__':
     r21 = False
@@ -77,8 +89,7 @@ if __name__ == '__main__':
 
 
 def set_difficulty(value, difficulty):
-
-    game_window.active_level = load_level()
+    game_window.active_level = set_level(value[0][0])
 
 
 def start_the_game():
