12564f64e0a845c53d832d8cae10f810e713354f
Wooster70 <86495410+Wooster70@users.noreply.github.com>
2022-04-18 15:49:23 +0300

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit faf01400526fb14be59e9cc6650e364b213ebfc8, reversing
    changes made to 2e273ac748200642ee65b0154f860c38d69b1223.

--- Web/forms.py
+++ Web/forms.py
@@ -5,18 +5,18 @@ from flask_wtf import FlaskForm, RecaptchaField
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Mail or username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember me')
-    submit = SubmitField('Enter')
+    email = StringField('Никнейм или почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 class RegisterForm(FlaskForm):
-    email = EmailField('Mail address', validators=[DataRequired()])
-    login = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password_again = PasswordField('Password again', validators=[DataRequired()])
-    about = TextAreaField("About you")
+    email = EmailField('Почта', validators=[DataRequired()])
+    login = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    about = TextAreaField("О себе")
     picture = FileField()
     recaptcha = RecaptchaField()
-    submit = SubmitField('Confirm')
+    submit = SubmitField('Зарегистрироваться')
